272a144cbd86435507d49dcbd6881a20b57f0b21
vishal payyawar <139535850+vishalseries7030@users.noreply.github.com>
2025-08-03 15:22:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 function validEmail(str) {
-  //your JS code here.
+  const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  return regex.test(str);
 }
 
 // Do not change the code below.
